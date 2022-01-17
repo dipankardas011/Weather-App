0eb5b3597bd8e80c03bc59b3fc4df6a6e809353d
DipankarDas <dipsonu10@hotmail.com>
2022-01-17 20:23:35 +0530

    New Layout

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-//const APP_ID = "__ENTER_YOUR_KEY_HERE__"
 const APP_ID = "5c6d087ebee8601a3b2d43a1a795205e"
-const apiUrl = `http://api.weatherstack.com/current?access_key=${APP_ID}&units=m`
+const apiUrl = `http://api.weatherstack.com/forecast?access_key=${APP_ID}&units=m`
 
-const errorFillup = (data, side) => {
-	if (side == 2) {
-		var classSelection = ".search-result"
-	}
+
+const errorFillup = (data) => {
+	
+	classSelection = ".search-result"
+
+	document.querySelector(`${classSelection}`).innerHTML = ""
+	document.querySelector(`${classSelection}`).insertAdjacentHTML(
+		"beforeend",
+		`<div class="error-message">
+			<div class="error-title">Error</div>
+			<div class="error-message">${data.error.code} : ${data.error.type}</div>
+		</div>`
+	)
+
+	classSelection = ".search-resultF"
 
 	document.querySelector(`${classSelection}`).innerHTML = ""
 	document.querySelector(`${classSelection}`).insertAdjacentHTML(
@@ -18,10 +28,9 @@ const errorFillup = (data, side) => {
 }
 
 
-const fillup = (data, side) => {
-	if (side == 2) {
-		var classSelection = ".search-result"
-	}
+const fillup = (data) => {
+
+	const classSelection = ".search-result"
 
 
 	document.querySelector(`${classSelection}`).innerHTML = ""
@@ -42,35 +51,92 @@ const fillup = (data, side) => {
 						<img src="${data.current.weather_icons[0]}" />
 					</div>
 					<div class="temp-details">
-						<div class="temp-curr">${data.current.temperature}° C</div>
+						<div class="temp-curr">${data.current.temperature} °C</div>
 						 <div class="temp-main-status">${data.current.weather_descriptions[0]} </div>
 					</div>
 				</div>
 				<div class="temp-properties">
 					<div class="temp max">
 						<div class="title">Current Temp </div>
-						<div class="content">${data.current.temperature}°C</div>
+						<div class="content">${data.current.temperature} °C</div>
 					</div>
 					
 					<div class="temp feels-like">
 						<div class="title">Feels Like</div>
-						<div class="content">${data.current.feelslike}°C</div>
+						<div class="content">${data.current.feelslike} °C</div>
 					</div>
-					<div class="pressure">
+					<div class="temp pressure">
 						<div class="title">pressure</div>
-						<div class="content">${data.current.pressure}mbar</div>
+						<div class="content">${data.current.pressure} mbar</div>
 					</div>
 					<div class="temp humidity">
 						<div class="title">Humidity</div>
-						<div class="content">${data.current.humidity}%</div>
+						<div class="content">${data.current.humidity} %</div>
 					</div>
 					<div class="temp wind-speed">
 						<div class="title">Wind Speed</div>
-						<div class="content">${data.current.wind_speed}m/s</div>
+						<div class="content">${data.current.wind_speed} m/s</div>
 					</div>
 					<div class="temp wind-deg">
 						<div class="title">Wind Direction</div>
-						<div class="content">${data.current.wind_dir}deg</div>
+						<div class="content">${data.current.wind_dir}</div>
+					</div>
+				</div>`
+	)
+}
+
+
+const fillupF = (dataF) => {
+
+	const classSelection = ".search-resultF"
+
+
+	document.querySelector(`${classSelection}`).innerHTML = ""
+
+	document.querySelector(`${classSelection}`).insertAdjacentHTML(
+		"beforeend",
+		`<div class="location-container">
+			<div class="icon-wrapper">
+						<i class="fas fa-thumbtack"></i>
+					</div>
+					<div class="location-details">
+						<div class="location-name">${dataF.location.name}</div>
+						<div class="location-coord">${dataF.location.lat}, ${dataF.location.lon}</div>
+					</div>
+				</div>
+				For date :: ${Object.keys(dataF.forecast)[0]}
+					<br>
+				<div class="temp-container">
+					
+					<div class="temp-details">
+						<div class="title">AvgTemp</div>
+						<div class="temp-curr">${dataF.forecast[Object.keys(dataF.forecast)[0]].avgtemp} °C</div>
+					</div>
+				</div>
+				<div class="temp-properties">
+					<div class="temp max">
+						<div class="title">Max Temp </div>
+						<div class="content">${dataF.forecast[Object.keys(dataF.forecast)[0]].maxtemp} °C</div>
+					</div>
+
+					<div class="temp min">
+						<div class="title">Min Temp </div>
+						<div class="content">${dataF.forecast[Object.keys(dataF.forecast)[0]].mintemp} °C</div>
+					</div>
+					
+					<div class="temp sunhour">
+						<div class="title">Sun hours</div>
+						<div class="content">${dataF.forecast[Object.keys(dataF.forecast)[0]].sunhour} hrs</div>
+					</div>
+
+					<div class="temp totalsnow">
+						<div class="title">Visibility</div>
+						<div class="content">${dataF.forecast[Object.keys(dataF.forecast)[0]].totalsnow} m</div>
+					</div>
+
+					<div class="temp uv index">
+						<div class="title">UV index</div>
+						<div class="content">${dataF.forecast[Object.keys(dataF.forecast)[0]].uv_index}</div>
 					</div>
 				</div>`
 	)
@@ -87,26 +153,31 @@ const showPosition = (position) => {
 			if (data.success === false) {
 				return
 			}
-			fillup(data, 1)
+			fillup(data)
 		})
-	document.querySelector(".input.location").focus()
+	document.querySelector(".inputLocation").focus()
 }
 
 const findWeatherbyCity = () => {
 	const city = document.querySelector(".inputLocation").value;
 	console.log(city)
+	// api call to get the current forecast
 	fetch(`${apiUrl}&query=${city}`)
 		.then((res) => res.json())
 		.then((data) => {
 			console.log(data)
+			console.log("Current -> ", data.current)
+			console.log("Future -> ", data.forecast[Object.keys(data.forecast)[0]])
+			
 			if (data.success === false) {
-				errorFillup(data, 2)
+				errorFillup(data)
 				return
 			}
-			console.log(data.current);
-			console.log(data.location);
-			fillup(data, 2)
+			fillup(data)
+			fillupF(data)
 		})
-	document.querySelector(".input.location").value = ""
-	document.querySelector(".input.location").focus()
+	
+	// api call to get the future forecast		
+	document.querySelector(".inputLocation").value = ""
+	document.querySelector(".inputLocation").focus()
 }
